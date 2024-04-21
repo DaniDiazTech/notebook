1086ec71a7f090e09da3b8f50d19d89051f4c145
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-04-21 17:34:45 -0500

    Added Movements in matrix

---
+++ Utilities/movements_2d.cpp
@@ -0,0 +1,7 @@
+/*
+Used for movements in a 2d matrix. Useful for Graph problems on graphs.
+Tested: https://codeforces.com/gym/518941/submission/257482117
+*/
+// L R U D LU LD RU RD
+int dr[] = {0, 0, -1, 1, -1, 1, -1, 1};
+int dc[] = {-1, 1, 0, 0, -1, -1, 1, 1};
