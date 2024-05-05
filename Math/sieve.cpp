b7075ec4a6c19040c406e14d60dd86a06923b72c
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-05-04 21:56:22 -0500

    Added Generate primes function

--- Math/sieve.cpp
+++ Math/sieve.cpp
@@ -6,17 +6,38 @@ Usage:
 Calculate primes in the range 1, ... , n
 Complexity: O(n ln ln n)
 */
-void sieve(vector<bool> &primes, int n){
-  primes.assign(n + 1, 1);
-  primes[0] = primes[1] = 0;
+void run_sieve(vector<bool> &sieve, int n){
+  sieve.assign(n + 1, 1);
+  sieve[0] = sieve[1] = 0;
 
-  for (int i = 2; i * i <= n; i++){
-    if (primes[i]){
-      for (int j = i * i; j <= n; j+=i){
-        primes[j] = false;
+  for (long long i = 2; i * i <= n; i++){
+    if (sieve[i]){
+      for (long long j = i * i; j <= n; j+=i){
+        sieve[j] = false;
       }
     }
   }
+}
+
+/*
+Create primes up to a number N.
+Complexity: O(n ln ln n)
+*/
+
+void gen_primes(vector<bool> &sieve, vector<int> &primes, int n){
+  sieve.assign(n + 1, 1);
+  primes.clear();
+  sieve[0] = sieve[1] = 0;
+
+  for (long long i = 2; i <= n; i++){
+    if (sieve[i]){
+      primes.push_back((int)i);
+      for (long long j = i * i; j <= n; j+=i){
+        sieve[j] = false;
+      }
+    }
+  }
+
 }
 
 
@@ -25,11 +46,19 @@ Example:
 */
 int main(){
   int n = 100;
-  vector<bool> primes;
-  sieve(primes, n);
+  vector<bool> sieve;
+  run_sieve(sieve, n);
   for (int i =0 ;i <= n; i++){
-    if (primes[i]){
+    if (sieve[i]){
       cout << i << endl;
     }
   }
+
+  int m = 1000000;
+  vector<int> primes;
+  gen_primes(sieve, primes, m);  
+  cout << primes.size() << '\n';
+  // for (auto p: primes){
+  //   cout << p << '\n';
+  // }
 }
