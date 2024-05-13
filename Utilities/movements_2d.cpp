02ab52911a83124311a2775129e4aec2bae996b3
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-05-13 11:02:49 -0500

    Added valid

--- Utilities/movements_2d.cpp
+++ Utilities/movements_2d.cpp
@@ -5,3 +5,9 @@ Tested: https://codeforces.com/gym/518941/submission/257482117
 // L R U D LU LD RU RD
 int dr[] = {0, 0, -1, 1, -1, 1, -1, 1};
 int dc[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+
+// N rows, M columns
+int n, m;
+bool valid(int i, int j){
+  return i >= 0 && j >= 0 && i < n && j < m;
+}
--- Utilities/pragma_optimizations.cpp
+++ Utilities/pragma_optimizations.cpp
@@ -5,3 +5,8 @@ with floats
 #pragma GCC optimize ("O3")
 #pragma GCC target ("sse4")
 #pragma GCC target ("avx,tune=native")
+
+
+// Other pragmas
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
