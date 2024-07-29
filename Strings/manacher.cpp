434aecd2af4107ba2eb6e632aca1456cb7167ff4
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-07-28 23:35:46 -0500

    Added elvasito manacher

---
+++ Strings/manacher.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6;
+int d1[N]; //d1[i] = max odd palindrome centered on i
+int d2[N]; //d2[i] = max even palindrome centered on i and i - 1
+
+//s  aabbaacaabbaa
+//d1 1111117111111
+//d2 0103010010301
+
+void manacher(string& s){
+	int l=0,r=-1,n=s.size();
+
+	for (int i = 0;i < n; i++){
+		int k=  i > r ? 1 :min(d1[l+r-i], r-i) ;
+		while(i+k<n&&i-k>=0&&s[i+k]==s[i-k])k++;
+		d1[i]=k--;
+		if(i+k>r)l=i-k,r=i+k;
+	}
+	l=0;r=-1;
+
+	for (int i =0 ;i < n; i++){
+		int k=i>r?0:min(d2[l+r-i+1],r-i+1);k++;
+		while(i+k<=n&&i-k>=0&&s[i+k-1]==s[i-k])k++;
+		d2[i]=--k;
+		if(i+k-1>r)l=i-k,r=i+k-1;
+	}
+}
