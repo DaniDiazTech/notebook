3c4958b061984ebb8ef6a0ef152ce0c93709a64b
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-09-23 10:35:44 -0500

    Added mos_algorithm with optimization

---
+++ Data-structures/hilbert_order_optimization.cpp
@@ -0,0 +1,46 @@
+/*
+Creates optimal ordering for
+mo's algorithm queries.
+
+Changes complexity from O((n + q)sqrt(n)) to: O(n*sqrt(q))
+
+Creates query objects as:
+queries[i].l = l;
+queries[i].r = r;
+queries[i].ind = i;
+queries[i].calcOrder();
+*/
+inline int64_t hilbertOrder(int x, int y, int pow, int rotate) {
+	if (pow == 0) {
+		return 0;
+	}
+	int hpow = 1 << (pow-1);
+	int seg = (x < hpow) ? (
+		(y < hpow) ? 0 : 3
+	) : (
+		(y < hpow) ? 1 : 2
+	);
+	seg = (seg + rotate) & 3;
+	const int rotateDelta[4] = {3, 0, 0, 1};
+	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+	int nrot = (rotate + rotateDelta[seg]) & 3;
+	int64_t subSquareSize = int64_t(1) << (2*pow - 2);
+	int64_t ans = seg * subSquareSize;
+	int64_t add = hilbertOrder(nx, ny, pow-1, nrot);
+	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+	return ans;
+}
+ 
+struct Query {
+	int l, r, ind;
+	int64_t ord;
+ 
+	inline void calcOrder() {
+		ord = hilbertOrder(l, r, 21, 0);
+	}
+};
+ 
+inline bool operator<(const Query &a, const Query &b) {
+	return a.ord < b.ord;
+}
+
---
+++ Data-structures/mos_algorithm.cpp
@@ -0,0 +1,46 @@
+/*
+Answer offlines queries based on an ordering that guarantees
+O((n + q)sqrt(n))
+
+Set block = sqrt(n)
+Uses global variables
+*/
+int block;
+struct Query{
+  int l, r, ind;
+
+  bool operator < (Query other) const{
+    return make_pair(l / block, r)  < make_pair(other.l/block, other.r);    
+  }
+};
+
+// Add and remove objects according to the problem
+void add(int i){
+}
+
+void remove(int i){
+}
+
+int get_ans(){
+}
+
+
+void mo(vector<Query> &queries){
+  sort(queries.begin(), queries.end());
+  vector<ll> ans(queries.size());
+  
+  int cur_l = queries[0].l, cur_r = queries[0].l - 1;
+  for (Query q: queries){
+    // Make cur_l = q.l and cur_r = q.r
+    while (cur_l < q.l) remove(cur_l++);
+    while (cur_l > q.l) add(--cur_l);
+
+    while(cur_r > q.r) remove(cur_r--);
+    while (cur_r < q.r) add(++cur_r);
+
+    ans[q.ind] = get_ans();
+  }
+
+  // Print answer
+  for (auto x: ans) cout << x << "\n";
+}
