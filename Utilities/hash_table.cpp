d66cf29588b438ffc51df9d4fc0b792e87dccb5f
Daniel Diaz <danieldiazgonza52@gmail.com>
2024-09-23 23:05:23 -0500

    Hash table

---
+++ Utilities/hash_table.cpp
@@ -0,0 +1,9 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+gp_hash_table<int, int> mp;
+
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) const { return x ^ RANDOM; }
+};
+gp_hash_table<int, int, chash> table;
