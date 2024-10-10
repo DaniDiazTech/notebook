53e918fa7887fbbf6320ef18660ed22b57c318cd
jnmrtnz111 <juamartinezh@unal.edu.co>
2024-10-10 12:15:27 -0500

    Add files via upload

---
+++ TEMPLATE_JuanDavid.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define forn(u,n) for(int u=0;u<n;++u)
+#define forns(u,i,n) for(int u=i;u<n;++u)
+#define todo0(a) memset(a,0,sizeof a)
+#define todom1(a) memset(a,-1,sizeof a)
+#define reverso int, vector<int>, greater<int>
+#define sz(a) ((int)a.size())
+#define N 100005
+#define pb push_back
+#define snd second
+#define fst first
+
+using namespace std;
+
+typedef long long ll;
+
+const unsigned long long  MOD = 1000567999;
+
+int main(){
+    #ifdef LOCAL
+    freopen("entra.in","r",stdin);
+    #endif
+    ios_base::sync_with_stdio();
+    cin.tie();
+}
